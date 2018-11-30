a8d9002f4db7803e54596dfaaf46f548e6f45256
k96payne <k96payne@gmail.com>
2018-11-30 11:11:32 -0500

    update profile

--- myStocks/target/myStocks-2.0.3.RELEASE/js/profile.js
+++ myStocks/target/myStocks-2.0.3.RELEASE/js/profile.js
@@ -17,20 +17,21 @@ function httpGetAsync(theUrl, callback) {
     xmlHttp.send(null);
 }
 
-function httpPutAsync(theUrl, callback) {
+function httpPutAsync(theUrl, requestObject, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            callback(200);
+        if (xmlHttp.readyState == 4 && xmlHttp.status < 300) {
+            callback(xmlHttp.status);
         } else if (xmlHttp.readyState == 4)
             callback(xmlHttp.status);
 
     }
     xmlHttp.open("PUT", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.send(JSON.stringify(requestObject));
 }
 
-httpGetAsync("/myStocks-2.0.3.RELEASE/store/customers/"+username, function(data){
+httpGetAsync("/myStocks-2.0.3.RELEASE/myStocks/users/"+username, function(data){
     console.log(data);
     var fname = data.fname;
     var lname = data.lname;
@@ -52,15 +53,23 @@ httpGetAsync("/myStocks-2.0.3.RELEASE/store/customers/"+username, function(data)
 })
 
 
-document.getElementById("sign-in").onclick = function () {
+document.getElementById("update").onclick = function () {
     var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
-    var url = "/myStocks-2.0.3.RELEASE/store/customers?fname=" + fname + "&lname=" + lname + "&username=" + username + "&email=" + email;
+    var url = "/myStocks-2.0.3.RELEASE/myStocks/users";
 
-    httpPutAsync(url, function (data) {
+    var requestObject = {};
+    requestObject.username = username;
+	requestObject.fname = fname;
+	requestObject.lname = lname;
+    requestObject.password = password;
+	requestObject.email = email;
+
+    httpPutAsync(url, requestObject, function (data) {
         console.log(data);
-        if (data != 200) {
+        if (data >= 300) {
             document.getElementById("targ").innerHTML = "Please try again....";
         } else {
             window.location.assign("/myStocks-2.0.3.RELEASE/views/profile.html")
@@ -82,63 +91,3 @@ function isLoggedIn() {
     console.log("Not logged in");
     return false;
 }
-
-httpGetAsync("/myStocks-2.0.3.RELEASE/store/carts?username=" + username, function (data) {
-    console.log(data);
-    for (var j = 0; j < data.length; i++) {
-        console.log("here")
-        var start = document.getElementById("myparet");
-        var head = document.createElement("h1");
-        var cartNum = data.cartId;
-        head.innerHTML = "Cart Number: " + cartNum;
-        start.appendChild(head);
-        for (var i = 0; i < data.items.length; i++) {
-            console.log(data);
-            item = data.items[i];
-            itemId = item.productId;
-            var child1 = document.createElement("div");
-            child1.id = "target" + itemId;
-            child1.classList.add("text-center");
-            head.appendChild(child1);
-            var title = document.createElement("h3");
-            title.classList.add("bold");
-            title.classList.add("text-dark");
-            title.innerHTML = item.productName;
-            child1.appendChild(title);
-            var price = document.createElement("p");
-            price.classList.add("italic");
-            price.innerHTML = "Price: <b><em>$ " + item.msrp + "</em></b>";
-            child1.appendChild(price);
-            var sale = document.createElement("h5");
-            sale.id = "sale";
-            sale.innerHTML = "SALE: $" + item.salePrice;
-            child1.appendChild(sale);
-
-            var card5 = document.createElement("a");
-            card5.onclick = function () {
-                if (isLoggedIn()) {
-                    var url = '/myStocks-2.0.3.RELEASE/store/carts?cartId=' + data.cartId + '&productId=' + itemId;
-                    httpDeleteAsync(url, null);
-                    var bye = document.getElementById("target" + itemId);
-                    bye.remove();
-                }
-            }
-            card5.classList.add("btn");
-            card5.classList.add("btn-primary");
-            card5.innerHTML = "<b>Remove from cart</b>";
-            child1.appendChild(card5);
-
-            var card6 = document.createElement("a");
-            card6.onclick = function () {
-                document.cookie = "itemId=" + item.itemId + ";path=/;"
-                window.location.assign("/myStocks-2.0.3.RELEASE/views/item.html")
-            }
-            card6.classList.add("btn");
-            card6.classList.add("btn-primary");
-            card6.innerHTML = "<b>View item</b>";
-            child1.appendChild(card6);
-
-        }
-    }
-
-});
